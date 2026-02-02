7b59c9a18a1698d100df18d9a9a0c6cc08935af8
Artem Suzdal <artemsuzdal@proton.me>
2026-02-02 21:25:28 +0100

    feat: track events, aggregate & visuzlize data in MongoDB

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,14 @@ import cors from 'cors'
 
 import { postsRoutes } from './routes/posts.js'
 import { userRoutes } from './routes/users.js'
+import { eventRoutes } from './routes/events.js'
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 postsRoutes(app)
 userRoutes(app)
+eventRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
