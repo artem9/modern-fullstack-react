205153b76c311dbff3a8397ab106cc29b0f78e48
Artem Suzdal <artemsuzdal@proton.me>
2026-02-24 20:03:09 +0100

    feat: interfacing with GraphQL on the Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import PropTypes from 'prop-types'
 import { HelmetProvider } from 'react-helmet-async'
 
+import { ApolloProvider } from '@apollo/client/react'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { HttpLink } from '@apollo/client/link/http'
+
 import { AuthContextProvider } from './providers/AuthContextProvider.jsx'
 
 const queryClient = new QueryClient()
+const apolloClient = new ApolloClient({
+  link: new HttpLink({ uri: import.meta.env.VITE_GRAPHQL_URL }),
+  cache: new InMemoryCache(),
+})
 
 export function App({ children }) {
   return (
     <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <AuthContextProvider>{children}</AuthContextProvider>
-      </QueryClientProvider>
+      <ApolloProvider client={apolloClient}>
+        <QueryClientProvider client={queryClient}>
+          <AuthContextProvider>{children}</AuthContextProvider>
+        </QueryClientProvider>
+      </ApolloProvider>
     </HelmetProvider>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import { useMutation } from '@tanstack/react-query'
 import { useNavigate, Link } from 'react-router-dom'
+import { useMutation as useGraphQLMutation } from '@apollo/client/react'
 
-import { login } from '../api/users.js'
 import { useAuth } from '../hooks/useAuth.js'
+import { LOGIN_USER } from '../api/graphql/users.js'
 
 export function Login() {
   const [, setToken] = useAuth()
@@ -11,10 +11,10 @@ export function Login() {
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
 
-  const loginMutation = useMutation({
-    mutationFn: () => login({ username, password }),
-    onSuccess: (data) => {
-      setToken(data.token)
+  const [loginUser, { loading }] = useGraphQLMutation(LOGIN_USER, {
+    variables: { username, password },
+    onCompleted: (data) => {
+      setToken(data.loginUser)
       navigate('/')
     },
     onError: () => alert('failed to login!'),
@@ -22,7 +22,7 @@ export function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    loginMutation.mutate()
+    loginUser()
   }
 
   return (
@@ -54,8 +54,8 @@ export function Login() {
       <br />
       <input
         type='submit'
-        value={loginMutation.isPending ? 'Logging in...' : 'Log In'}
-        disabled={!username || !password || loginMutation.isPending}
+        value={loading ? 'Logging in...' : 'Log In'}
+        disabled={!username || !password || loading}
       />
     </form>
   )
